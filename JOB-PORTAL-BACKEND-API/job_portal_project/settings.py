ea4d3abed3914d2113ce587655dfedec2b032fc9
AMAL-C-K <amalckg1999@gmail.com>
2026-09-16 01:09:06 +0530

    Update settings.py

--- JOB-PORTAL-BACKEND-API/job_portal_project/settings.py
+++ JOB-PORTAL-BACKEND-API/job_portal_project/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_upr50759-f2cgpjyf#=l$r1@yj*!@qq3%_zi4(5fy1bqggit2'
 
 SECRET_KEY = os.getenv(
     "SECRET_KEY",
