b323cca00109255de6851551eb0424b72553c7d9
AMAL-C-K <amalckg1999@gmail.com>
2026-09-16 01:55:16 +0530

    Update settings.py

--- JOB-PORTAL-BACKEND-API/job_portal_project/settings.py
+++ JOB-PORTAL-BACKEND-API/job_portal_project/settings.py
@@ -172,5 +172,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://job-portal-backend-q9m0.onrender.com/',
+    'https://job-portal-backend-q9m0.onrender.com',
 ]
