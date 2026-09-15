fe69d9162abd06d557382b6950d9297f3c85b9ac
AMAL-C-K <amalckg1999@gmail.com>
2026-09-16 00:56:04 +0530

    Update build.sh

--- JOB-PORTAL-BACKEND-API/build.sh
+++ JOB-PORTAL-BACKEND-API/build.sh
@@ -1,1 +1,7 @@
+#!/usr/bin/env bash
 
+python manage.py migrate
+
+python manage.py collectstatic --noinput
+
+python manage.py shell -c "from accounts.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', '1234')"
