31dcbd20c53275820e9186dcd99fccb63094edc6
AMAL-C-K <amalckg1999@gmail.com>
2026-09-16 20:50:21 +0530

    Update build.sh

--- JOB-PORTAL-BACKEND-API/build.sh
+++ JOB-PORTAL-BACKEND-API/build.sh
@@ -4,3 +4,15 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@gmail.com',
+        password='1234'
+    )
+"
+
