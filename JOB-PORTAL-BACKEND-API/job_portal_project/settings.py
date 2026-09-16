e5cc5cb3f280f5ba1a6a6b423e5c48fda32287f6
AMAL-C-K <amalckg1999@gmail.com>
2026-09-16 20:21:57 +0530

    Update settings.py

--- JOB-PORTAL-BACKEND-API/job_portal_project/settings.py
+++ JOB-PORTAL-BACKEND-API/job_portal_project/settings.py
@@ -170,7 +170,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # React development server
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'https://frontend-job-portal-dwul.onrender.com/',
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://frontend-job-portal-dwul.onrender.com",
 ]
