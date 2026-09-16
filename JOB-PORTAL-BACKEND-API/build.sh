1b5d3c8cee611ae9e9661659e2edd3b29ee99d7c
AMAL-C-K <amalckg1999@gmail.com>
2026-09-16 15:16:44 +0530

    Update build.sh

--- JOB-PORTAL-BACKEND-API/build.sh
+++ JOB-PORTAL-BACKEND-API/build.sh
@@ -4,4 +4,3 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-python manage.py shell -c "import os; from accounts.models import User; username=os.environ.get('DJANGO_SUPERUSER_USERNAME'); email=os.environ.get('DJANGO_SUPERUSER_EMAIL'); password=os.environ.get('DJANGO_SUPERUSER_PASSWORD'); User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
